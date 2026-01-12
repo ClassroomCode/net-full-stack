b146e0e117356434da1809a5562acd9543ff5940
Jason Bell <jason.bell@treeloop.com>
2026-01-12 16:01:07 -0500

    Initial labs

---
+++ labs/Playground/Playground/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Playground
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = new List<int>() { 1, 2, 3, 4, 5, 6, 7 };
+
+            var evens = nums.Where(n => n % 2 == 0).ToArray();
+
+            foreach (var e in evens)
+            {
+                Console.WriteLine(e);
+            }
+
+            nums.Add(12);
+            Console.WriteLine("------------");
+
+            foreach (var e in evens)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
