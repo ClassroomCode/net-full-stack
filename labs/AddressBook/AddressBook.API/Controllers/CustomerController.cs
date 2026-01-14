3bce7492f54632be07562bd0746188e7d0ec09f5
Jason Bell <jason.bell@treeloop.com>
2026-01-14 08:54:21 -0500

    CustomerController

---
+++ labs/AddressBook/AddressBook.API/Controllers/CustomerController.cs
@@ -0,0 +1,26 @@
+﻿using AddressBook.API.DataAccess;
+using AddressBook.API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AddressBook.API.Controllers;
+
+[ApiController]
+public class CustomerController : ControllerBase
+{
+    [HttpGet("customer")]
+    public ActionResult GetAllCustomers()
+    {
+        using var db = new NorthwindContext();
+        var customers = db.Customers.ToList();
+        return Ok(customers);
+    }
+
+    [HttpGet("customer/{id}")]
+    public ActionResult GetCustomer(string id)
+    {
+        using var db = new NorthwindContext();
+        var customer = db.Customers.Find(id);
+        if (customer is null) return NotFound();
+        return Ok(customer);
+    }
+}
--- labs/AddressBook/AddressBook.API/Program.cs
+++ labs/AddressBook/AddressBook.API/Program.cs
@@ -1,21 +1,28 @@
 using AddressBook.API.DataAccess;
-using Microsoft.AspNetCore.Http.HttpResults;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
-app.MapGet("/customer", () => {
+app.MapControllers();
+
+/*
+app.MapGet("/customer", () =>
+{
     using var db = new NorthwindContext();
     var customers = db.Customers.ToList();
     return customers;
 });
 
-app.MapGet("/customer/{id}", (string id) => {
+app.MapGet("/customer/{id}", (string id) =>
+{
     using var db = new NorthwindContext();
     var customer = db.Customers.Find(id);
     if (customer is null) return Results.NotFound();
     return Results.Ok(customer);
 });
+*/
 
 app.Run();
