f565ad03bb388e6a775088739f22f98e3eb51b2f
Jason Bell <jason.bell@treeloop.com>
2026-01-15 11:16:43 -0500

    Order controller

---
+++ labs/AddressBook/AddressBook.API/Controllers/OrderController.cs
@@ -0,0 +1,27 @@
+﻿using AddressBook.API.DataAccess;
+using AddressBook.API.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AddressBook.API.Controllers;
+
+[ApiController]
+public class OrderController(NorthwindContext db) : ControllerBase
+{
+    [HttpGet("order")]
+    public ActionResult GetAllOrders()
+    {
+        var orders = db.Orders
+            .Include(o => o.Customer)
+            .Select(o => new {
+                OrderId = o.OrderID,
+                ShippedDate = o.ShippedDate,
+                OrderDate = o.OrderDate,
+                CustomerName = o.Customer!.CompanyName
+                })
+            .ToList();
+
+        return Ok(orders);
+    }
+}
